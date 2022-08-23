77cad9069a58ffbaf3f2992af3f00c4afdfd6196
Alec Delaney <tekktik@gmail.com>
2022-08-22 21:36:31 -0400

    Keep copyright up to date in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@
 #
 import os
 import sys
+import datetime
 
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -58,6 +59,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Adafruit PCF8523 RTC Library"
+current_year = str(datetime.datetime.now().year)
 copyright = "2016, Philip Moyer"
 author = "Philip Moyer"
 
