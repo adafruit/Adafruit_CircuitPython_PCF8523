72139e7c67a7d50773f060eb33ecfe55976f65a4
Bernhard Bablok <bablokb@gmx.de>
2023-01-28 10:47:52 +0100

    implement timer-support

--- adafruit_pcf8523.py
+++ adafruit_pcf8523.py
@@ -39,6 +39,7 @@ Implementation Notes
 **Notes:**
 
 #. Milliseconds are not supported by this RTC.
+#. The alarm does not support seconds. It will always fire on full minutes.
 #. Datasheet: http://cache.nxp.com/documents/data_sheet/PCF8523.pdf
 
 """
@@ -51,6 +52,7 @@ from adafruit_register import i2c_bit
 from adafruit_register import i2c_bits
 from adafruit_register import i2c_bcd_alarm
 from adafruit_register import i2c_bcd_datetime
+from micropython       import const
 
 try:
     import typing  # pylint: disable=unused-import
@@ -106,7 +108,7 @@ class PCF8523:
 
     power_management = i2c_bits.RWBits(3, 0x02, 5)
     """Power management state that dictates battery switchover, power sources
-    and low battery detection. Defaults to BATTERY_SWITCHOVER_OFF (0b000)."""
+    and low battery detection. Defaults to BATTERY_SWITCHOVER_OFF (0b111)."""
 
     # The False means that day comes before weekday in the registers. The 0 is
     # that the first day of the week is value 0 and not 1.
@@ -126,18 +128,123 @@ class PCF8523:
     111 - Disabled
     """
 
+    CLOCKOUT_FREQ_32KHZ    = const(0b000)
+    """Clock frequency of 32 KHz"""
+    CLOCKOUT_FREQ_16KHZ    = const(0b001)
+    """Clock frequency of 16 KHz"""
+    CLOCKOUT_FREQ_8KHZ     = const(0b010)
+    """Clock frequency of  8 KHz"""
+    CLOCKOUT_FREQ_4KHZ     = const(0b011)
+    """Clock frequency of  4 KHz"""
+    CLOCKOUT_FREQ_1KHZ     = const(0b100)
+    """Clock frequency of  4 KHz"""
+    CLOCKOUT_FREQ_32HZ     = const(0b101)
+    """Clock frequency of 32 Hz"""
+    CLOCKOUT_FREQ_1HZ      = const(0b110)
+    """Clock frequency of 1 Hz"""
+    CLOCKOUT_FREQ_DISABLED = const(0b111)
+    """Clock output disabled"""
+
     # The False means that day and weekday share a register. The 0 is that the
     # first day of the week is value 0 and not 1.
     alarm = i2c_bcd_alarm.BCDAlarmTimeRegister(
         0x0A, has_seconds=False, weekday_shared=False, weekday_start=0
     )
-    """Alarm time for the first alarm."""
+    """Alarm time for the first alarm. Note that the value of the seconds-fields
+    is ignored, i.e. alarms only fire at full minutes. For short-term
+    alarms, use a timer instead."""
 
     alarm_interrupt = i2c_bit.RWBit(0x00, 1)
     """True if the interrupt pin will output when alarm is alarming."""
 
     alarm_status = i2c_bit.RWBit(0x01, 3)
     """True if alarm is alarming. Set to False to reset."""
+
+    timerA_enabled = i2c_bits.RWBits(2, 0x0F, 1)
+    """Configures timerA. Possible values:
+    00 - disabled
+    01 - enabled as countdown timer
+    10 - enabled as watchdog timer
+    11 - disabled
+    """
+
+    timerA_frequency = i2c_bits.RWBits(3, 0x10, 0)
+    """TimerA clock frequency. Default is 1/3600Hz.
+    Possible values are as shown (selection value - frequency).
+    000 - 4.096kHz
+    001 - 64Hz
+    010 -  1Hz
+    011 -  1/60Hz
+    111 -  1/3600Hz
+    """
+
+    TIMER_FREQ_4KHZ      = const(0b000)
+    """Timer frequency of 4 KHz"""
+    TIMER_FREQ_64HZ      = const(0b001)
+    """Timer frequency of 64 Hz"""
+    TIMER_FREQ_1HZ       = const(0b010)
+    """Timer frequency of 1 Hz"""
+    TIMER_FREQ_1_60HZ    = const(0b011)
+    """Timer frequency of 1/60 Hz"""
+    TIMER_FREQ_1_3600HZ  = const(0b111)
+    """Timer frequency of 1/3600 Hz"""
+
+    timerA_value = i2c_bits.RWBits(7, 0x11, 0)
+    """ TimerA value (0-255). The default is undefined.
+    The total countdown duration is calcuated by
+    timerA_value/timerA_frequency. For a higher precision, use higher values
+    and frequencies, e.g. for a one minute timer you could use
+    value=1, frequency=1/60Hz or value=60, frequency=1Hz. The
+    latter will give better results. See the PCF85x3 User's Manual
+    for details."""
+
+    timerA_interrupt = i2c_bit.RWBit(0x01, 1)
+    """True if the interrupt pin will assert when timerA has elapsed.
+    Defaults to False."""
+
+    timerA_watchdog = i2c_bit.RWBit(0x01, 2)
+    """True if the interrupt pin will output when timerA generates a
+    watchdog-alarm. Defaults to False."""
+
+    timerA_status = i2c_bit.RWBit(0x01, 6)
+    """True if timerA has elapsed. Set to False to reset."""
+
+    timerA_pulsed = i2c_bit.RWBit(0x0F, 7)
+    """True if timerA asserts INT as a pulse. The default
+    value False asserts INT permanently."""
+
+    timerB_enabled = i2c_bit.RWBit(0x0F, 0)
+    """True if the timerB is enabled. Default is False."""
+
+    timerB_frequency = i2c_bits.RWBits(3, 0x12, 0)
+    """TimerB clock frequency. Default is 1/3600Hz.
+    Possible values are as shown (selection value - frequency).
+    000 - 4.096kHz
+    001 - 64Hz
+    010 -  1Hz
+    011 -  1/60Hz
+    111 -  1/3600Hz
+    """
+
+    timerB_value = i2c_bits.RWBits(7, 0x13, 0)
+    """ TimerB value (0-255). The default is undefined.
+    The total countdown duration is calcuated by
+    timerB_value/timerB_frequency. For a higher precision, use higher values
+    and frequencies, e.g. for a one minute timer you could use
+    value=1, frequency=1/60Hz or value=60, frequency=1Hz. The
+    latter will give better results. See the PCF85x3 User's Manual
+    for details."""
+
+    timerB_interrupt = i2c_bit.RWBit(0x01, 0)
+    """True if the interrupt pin will assert when timerB has elapsed.
+    Defaults to False."""
+
+    timerB_status = i2c_bit.RWBit(0x01, 5)
+    """True if timerB has elapsed. Set to False to reset."""
+
+    timerB_pulsed = i2c_bit.RWBit(0x0F, 6)
+    """True if timerB asserts INT as a pulse. The default
+    value False asserts INT permanently."""
 
     battery_low = i2c_bit.ROBit(0x02, 2)
     """True if the battery is low and should be replaced."""
@@ -158,17 +265,6 @@ class PCF8523:
 
     def __init__(self, i2c_bus: I2C):
         self.i2c_device = I2CDevice(i2c_bus, 0x68)
-
-        # Try and verify this is the RTC we expect by checking the timer B
-        # frequency control bits which are 1 on reset and shouldn't ever be
-        # changed.
-        buf = bytearray(2)
-        buf[0] = 0x12
-        with self.i2c_device as i2c:
-            i2c.write_then_readinto(buf, buf, out_end=1, in_start=1)
-
-        if (buf[1] & 0b00000111) != 0b00000111:
-            raise ValueError("Unable to find PCF8523 at i2c address 0x68.")
 
     @property
     def datetime(self) -> struct_time:
