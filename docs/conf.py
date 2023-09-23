ccfdf67f54170d2d7b7d176c083e4c602b3e8833
Bernhard Bablok <bablokb@gmx.de>
2023-09-23 10:58:50 +0200

    ported support-files

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.intersphinx"]
 # Uncomment the below if you use native CircuitPython modules such as
 # digitalio, micropython and busio. List the modules you use. Without it, the
 # autodoc module docs will fail to generate with a warning.
-# autodoc_mock_imports = ["adafruit_bus_device", "adafruit_register"]
+autodoc_mock_imports = ["adafruit_bus_device", "adafruit_register"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
