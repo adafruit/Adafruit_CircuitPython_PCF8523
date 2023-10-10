f1b8c604b3e1111616be6f0a81462f3e5722ddf7
Bernhard Bablok <bablokb@gmx.de>
2023-10-10 15:08:04 +0200

    renamed timerA->timer, added register names

--- adafruit_pcf8523/timer.py
+++ adafruit_pcf8523/timer.py
@@ -13,6 +13,10 @@ This class supports the timer of the PCF8523-based RTC in CircuitPython.
 Functions are included for reading and writing registers and manipulating
 timer objects.
 
+The  PCF8523 support two timers named Tmr_A and Tmr_B in the datasheet.
+For compatibility with the PCF8563, the Tmr_A is named timer while the
+second timer is named timerB.
+
 The class supports stand-alone usage. In this case, pass an i2-bus object
 to the constructor. If used together with the PCF8523 class (rtc), instantiate
 the rtc-object first and then pass the i2c_device attribute of the rtc
@@ -70,15 +74,15 @@ class Timer:  # pylint: disable=too-few-public-methods
     :param I2C i2c_bus: The I2C bus object
     """
 
-    timerA_enabled = i2c_bits.RWBits(2, 0x0F, 1)
-    """Configures timerA. Possible values:
+    timer_enabled = i2c_bits.RWBits(2, 0x0F, 1)  # TAC[1:0]
+    """Configures timer. Possible values:
     00 - disabled
     01 - enabled as countdown timer
     10 - enabled as watchdog timer
     11 - disabled
     """
 
-    timerA_frequency = i2c_bits.RWBits(3, 0x10, 0)
+    timer_frequency = i2c_bits.RWBits(3, 0x10, 0)  # TAQ[2:0]
     """TimerA clock frequency. Default is 1/3600Hz.
     Possible values are as shown (selection value - frequency).
     000 - 4.096kHz
@@ -99,34 +103,34 @@ class Timer:  # pylint: disable=too-few-public-methods
     TIMER_FREQ_1_3600HZ = const(0b111)
     """Timer frequency of 1/3600 Hz"""
 
-    timerA_value = i2c_bits.RWBits(8, 0x11, 0)
+    timer_value = i2c_bits.RWBits(8, 0x11, 0)  # T_A[7:0]
     """ TimerA value (0-255). The default is undefined.
     The total countdown duration is calcuated by
-    timerA_value/timerA_frequency. For a higher precision, use higher values
+    timer_value/timer_frequency. For a higher precision, use higher values
     and frequencies, e.g. for a one minute timer you could use
     value=1, frequency=1/60Hz or value=60, frequency=1Hz. The
     latter will give better results. See the PCF85x3 User's Manual
     for details."""
 
-    timerA_interrupt = i2c_bit.RWBit(0x01, 1)
-    """True if the interrupt pin will assert when timerA has elapsed.
+    timer_interrupt = i2c_bit.RWBit(0x01, 1)  # CTAIE
+    """True if the interrupt pin will assert when timer has elapsed.
     Defaults to False."""
 
-    timerA_watchdog = i2c_bit.RWBit(0x01, 2)
-    """True if the interrupt pin will output when timerA generates a
+    timer_watchdog = i2c_bit.RWBit(0x01, 2)  # WTAIE
+    """True if the interrupt pin will output when timer generates a
     watchdog-alarm. Defaults to False."""
 
-    timerA_status = i2c_bit.RWBit(0x01, 6)
-    """True if timerA has elapsed. Set to False to reset."""
+    timer_status = i2c_bit.RWBit(0x01, 6)  # CTAF
+    """True if timer has elapsed. Set to False to reset."""
 
-    timerA_pulsed = i2c_bit.RWBit(0x0F, 7)
-    """True if timerA asserts INT as a pulse. The default
+    timer_pulsed = i2c_bit.RWBit(0x0F, 7)  # TAM
+    """True if timer asserts INT as a pulse. The default
     value False asserts INT permanently."""
 
-    timerB_enabled = i2c_bit.RWBit(0x0F, 0)
+    timerB_enabled = i2c_bit.RWBit(0x0F, 0)  # TBC
     """True if the timerB is enabled. Default is False."""
 
-    timerB_frequency = i2c_bits.RWBits(3, 0x12, 0)
+    timerB_frequency = i2c_bits.RWBits(3, 0x12, 0)  # TBQ[2:0]
     """TimerB clock frequency. Default is 1/3600Hz.
     Possible values are as shown (selection value - frequency).
     000 - 4.096kHz
@@ -136,7 +140,7 @@ class Timer:  # pylint: disable=too-few-public-methods
     111 -  1/3600Hz
     """
 
-    timerB_value = i2c_bits.RWBits(8, 0x13, 0)
+    timerB_value = i2c_bits.RWBits(8, 0x13, 0)  # T_B[7:0]
     """ TimerB value (0-255). The default is undefined.
     The total countdown duration is calcuated by
     timerB_value/timerB_frequency. For a higher precision, use higher values
@@ -145,14 +149,14 @@ class Timer:  # pylint: disable=too-few-public-methods
     latter will give better results. See the PCF85x3 User's Manual
     for details."""
 
-    timerB_interrupt = i2c_bit.RWBit(0x01, 0)
+    timerB_interrupt = i2c_bit.RWBit(0x01, 0)  # CTBIE
     """True if the interrupt pin will assert when timerB has elapsed.
     Defaults to False."""
 
-    timerB_status = i2c_bit.RWBit(0x01, 5)
+    timerB_status = i2c_bit.RWBit(0x01, 5)  # CTBF
     """True if timerB has elapsed. Set to False to reset."""
 
-    timerB_pulsed = i2c_bit.RWBit(0x0F, 6)
+    timerB_pulsed = i2c_bit.RWBit(0x0F, 6)  # TBM
     """True if timerB asserts INT as a pulse. The default
     value False asserts INT permanently."""
 
